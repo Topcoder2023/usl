35d0d465bd75dd5f0a3ee7ed7fa3860a880aa0fe
李鸿达 <hongda.li@hand-china.com>
2023-09-13 09:54:26 +0800

    [IMP] 完善注释

--- src/main/java/com/gitee/usl/infra/time/FixedDelayTimedTask.java
+++ src/main/java/com/gitee/usl/infra/time/FixedDelayTimedTask.java
@@ -8,6 +8,10 @@ package com.gitee.usl.infra.time;
 public interface FixedDelayTimedTask extends TimedTask {
     /**
      * 延迟时间
+     * 例如：delay() 返回为 5，且 unit 为 SECONDS
+     * 则每个任务执行完成后开始计时
+     * 五秒后执行下一个任务
+     * 因此每个任务的执行时间间隔是固定的，但是每个任务的执行频率不固定
      *
      * @return 延迟时间
      */
--- src/main/java/com/gitee/usl/infra/time/FixedRateTimedTask.java
+++ src/main/java/com/gitee/usl/infra/time/FixedRateTimedTask.java
@@ -8,6 +8,11 @@ package com.gitee.usl.infra.time;
 public interface FixedRateTimedTask extends TimedTask {
     /**
      * 执行周期
+     * 例如：cycle() 返回 10，且 unit 为 SECONDS
+     * 则每个任务执行开始后开始计时
+     * 10秒后执行下一个任务
+     * 即使当前任务没有执行完成也会进入下一个周期
+     * 因此每个任务的执行频率是固定的，但是任务之前的时间间隔不固定
      *
      * @return 执行周期数
      */
