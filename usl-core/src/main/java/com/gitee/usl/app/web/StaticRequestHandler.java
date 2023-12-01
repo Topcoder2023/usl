b2bcac573f937bcbdd874c75d6374779c654d569
李鸿达 <hongda.li.dlmu@qq.com>
2023-12-01 18:01:53 +0800

    [ADD] 新增WEB模板

--- usl-core/src/main/java/com/gitee/usl/app/web/StaticRequestHandler.java
+++ usl-core/src/main/java/com/gitee/usl/app/web/StaticRequestHandler.java
@@ -4,6 +4,8 @@ import cn.hutool.core.io.resource.ResourceUtil;
 import cn.hutool.core.text.CharSequenceUtil;
 import cn.hutool.core.text.StrPool;
 import com.google.auto.service.AutoService;
+import org.smartboot.http.common.enums.HeaderNameEnum;
+import org.smartboot.http.common.utils.Mimetypes;
 import org.smartboot.http.server.HttpRequest;
 import org.smartboot.http.server.HttpResponse;
 
@@ -15,8 +17,9 @@ import java.util.Optional;
 @AutoService(AbstractWebHandler.class)
 public class StaticRequestHandler extends AbstractWebHandler {
     private static final String PATH_PREFIX = "/static";
-    private static final String PATH = PATH_PREFIX + "/*";
+    private static final String PATH = PATH_PREFIX + "/**";
     private static final String DEFAULT_FILE_TYPE = ".html";
+    private static final String CONTENT_TYPE_SUFFIX = "; charset=utf-8";
 
     @Override
     public String getRoute() {
@@ -33,6 +36,9 @@ public class StaticRequestHandler extends AbstractWebHandler {
         } else {
             resourceName = suffix + DEFAULT_FILE_TYPE;
         }
+
+        String contentType = Mimetypes.getInstance().getMimetype(resourceName);
+        response.setHeader(HeaderNameEnum.CONTENT_TYPE.getName(), contentType + CONTENT_TYPE_SUFFIX);
 
         Optional.ofNullable(ResourceUtil.getStreamSafe(resourceName)).ifPresent(stream -> writeToStream(stream, response));
     }
