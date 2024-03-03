f24248ccba1c3ab96ecd66d97de6247a492945a4
宋嘉豪 <874416635@qq.com>
2024-03-03 14:47:45 +0800

    [ADD]新增消息队列

--- usl-function-data/src/test/java/com/gitee/usl/function/db/MessageQueueFunctionTest.java
+++ usl-function-data/src/test/java/com/gitee/usl/function/db/MessageQueueFunctionTest.java
@@ -1,26 +1,29 @@
 package com.gitee.usl.function.db;
 
 import com.gitee.usl.USLRunner;
-import com.gitee.usl.infra.DatabaseConstant;
 import com.gitee.usl.domain.Param;
+import com.gitee.usl.infra.DatabaseConstant;
 import com.gitee.usl.plugin.enhancer.FunctionMetaEnhancer;
+import org.junit.jupiter.api.Test;
 import org.slf4j.event.Level;
 import org.sqlite.core.NativeDB;
 
-import java.sql.SQLException;
-
-
-/**
- * @author hongda.li
- */
-class ConnectFunctionTest {
-
+public class MessageQueueFunctionTest {
     static USLRunner runner = new USLRunner()
             .configure(configuration -> configuration.put(DatabaseConstant.ENABLE_LOCK_KEY, true))
+            .configure(configuration -> configuration.put(DatabaseConstant.ENABLE_MQ_KEY, true))
             .configure(configuration -> configuration.enhancer(new FunctionMetaEnhancer()))
             .configure(configuration -> configuration.loggerLevel(NativeDB.class.getName(), Level.TRACE));
 
-    public static void main(String[] args) throws SQLException {
-        Object data = runner.run(new Param("date_value('02-2024-19','MM-yyyy-dd')")).getData();
+    @Test
+    void mq_produce() {
+        Object data = runner.run(new Param("produce_message('message1');" +
+                "produce_message('message2');" +
+                "produce_message('message3');")).getData();
+    }
+
+    @Test
+    void mq_consume() {
+        Object data = runner.run(new Param("consume_message(2)")).getData();
     }
 }
