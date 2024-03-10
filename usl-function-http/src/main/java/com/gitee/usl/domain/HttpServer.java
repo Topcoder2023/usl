dc33789b644f53ddc857d993eb66c76f16c6ee4e
曾婧姝 <597535178@qq.com>
2024-03-10 23:49:38 +0800

    [ADD]HttpServer中新增接收前端传入的参数功能(url参数、body参数、json参数等)

--- usl-function-http/src/main/java/com/gitee/usl/domain/HttpServer.java
+++ usl-function-http/src/main/java/com/gitee/usl/domain/HttpServer.java
@@ -1,7 +1,14 @@
 package com.gitee.usl.domain;
 
 import cn.hutool.core.convert.Convert;
+import cn.hutool.core.io.IoUtil;
 import cn.hutool.core.net.NetUtil;
+
+import cn.hutool.core.util.CharsetUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.util.URLUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import com.gitee.usl.USLRunner;
 import com.gitee.usl.api.WebRoute;
 import com.gitee.usl.api.annotation.Accessible;
@@ -13,48 +20,33 @@ import org.smartboot.http.server.HttpBootstrap;
 import org.smartboot.http.server.HttpRequest;
 import org.smartboot.http.server.HttpResponse;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.function.Consumer;
-
 /**
- * @author hongda.li
+ * @author hongda.li, jingshu.zeng
  */
 @Slf4j
 public class HttpServer {
-    /**
-     * 内置变量名称
-     */
     private static final String REQUEST = "request";
-    /**
-     * 内置变量名称
-     */
     private static final String RESPONSE = "response";
-    /**
-     * 端口号
-     */
+
     @Getter
     private final int port;
-    /**
-     * 本机地址
-     */
+
     @Getter
     private final String host;
-    /**
-     * 服务器名称
-     */
+
     @Getter
     private final String serverName;
-    /**
-     * 服务器代理
-     */
+
     private final HttpBootstrap proxy;
-    /**
-     * 执行器
-     */
+
     private final USLRunner runner;
-    /**
-     * 路由映射
-     */
+
     private final StringMap<WebRoute> routeMapping = new StringMap<>();
 
     public HttpServer(int port, USLRunner runner) {
@@ -89,6 +81,8 @@ public class HttpServer {
         this.addRoute(path, resource, WebRoute::filter);
     }
 
+
+
     @Accessible
     public void handler(String path, String resource) {
         log.info("HTTP服务处理器添加成功 - [{} ~ {}]", path, resource);
@@ -102,6 +96,19 @@ public class HttpServer {
             @Override
             public Boolean doHandle(HttpRequest request, HttpResponse response) {
                 ExecutableParam param = new ExecutableParam(runner, new ResourceParam(resource));
+
+                // 解析URL参数
+                Map<String, String> urlParams = parseUrlParams(request.getQueryString());
+                param.addContext("urlParams", urlParams);
+
+                // 解析JSON参数
+                JSONObject jsonObj = parseJsonParams(request);
+                param.addContext("jsonParams", jsonObj);
+
+                // 解析Body参数
+                Map<String, String> bodyParams = parseBodyParams(request);
+                param.addContext("bodyParams", bodyParams);
+
                 HttpRequestWrapper requestWrapper = new HttpRequestWrapper();
                 requestWrapper.set(request);
                 param.addContext(REQUEST, requestWrapper);
@@ -120,4 +127,68 @@ public class HttpServer {
     private String generateName() {
         return this.host + ":" + this.port;
     }
+
+    private Map<String, String> parseUrlParams(String queryString) {
+        Map<String, String> params = new HashMap<>();
+        if (StrUtil.isNotBlank(queryString)) {
+            String decodedQuery = URLUtil.decode(queryString);
+            for (String param : decodedQuery.split("&")) {
+                String[] pair = param.split("=");
+                String key = pair[0];
+                String value = pair.length > 1 ? pair[1] : "";
+                params.put(key, value);
+            }
+        }
+        return params;
+    }
+
+    private JSONObject parseJsonParams(HttpRequest request) {
+        JSONObject jsonObj = null;
+        try {
+            // 从请求体输入流中读取数据
+            InputStream inputStream = request.getInputStream();
+            if (inputStream != null) {
+                String bodyString = IoUtil.read(inputStream, CharsetUtil.CHARSET_UTF_8);
+                // 解析 JSON 字符串为 JSONObject 对象
+                jsonObj = JSONUtil.parseObj(bodyString);
+            }
+        } catch (IOException e) {
+            log.error("解析JSON参数时发生IO异常：", e);
+        } catch (Exception e) {
+            log.error("解析JSON参数时发生异常：", e);
+        }
+        return jsonObj;
+    }
+
+    private Map<String, String> parseBodyParams(HttpRequest request) {
+        Map<String, String> bodyParams = new HashMap<>();
+        String contentType = request.getContentType();
+        try {
+            if (StrUtil.isNotBlank(contentType) && contentType.toLowerCase().contains("application/json")) {
+                // JSON格式的参数获取
+                String bodyString = IoUtil.read(request.getInputStream(), CharsetUtil.CHARSET_UTF_8);
+                JSONObject jsonObj = JSONUtil.parseObj(bodyString);
+                for (Map.Entry<String, Object> entry : jsonObj.entrySet()) {
+                    bodyParams.put(entry.getKey(), entry.getValue().toString());
+                }
+            } else if (StrUtil.isNotBlank(contentType) && contentType.toLowerCase().contains("application/x-www-form-urlencoded")) {
+                // 表单形式的参数获取
+                Map<String, String[]> params = request.getParameters();
+                for (Map.Entry<String, String[]> entry : params.entrySet()) {
+                    if (entry.getValue() != null && entry.getValue().length > 0) {
+                        bodyParams.put(entry.getKey(), entry.getValue()[0]);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            log.error("解析请求的body参数时发生IO异常：", e);
+        } catch (Exception e) {
+            log.error("解析请求的body参数时发生异常：", e);
+        }
+        return bodyParams;
+    }
+
+
+
+
 }
